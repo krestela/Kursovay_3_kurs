550e8e6f62f6ba2380b81f71da23f0bfe82746b7
Kristina Adigamova <kriwowak@yandex.ru>
2023-02-08 15:19:41 +0500

    Initial commit

---
+++ src/main/java/com/example/kursovay_3_kurs/Kursovay3KursApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursovay_3_kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovay3KursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovay3KursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursovay_3_kurs/Kursovay3KursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovay_3_kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovay3KursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
